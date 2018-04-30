ddb04f2e49faa29339345c95dd6190881905fb27
abhi0002 <abhipsa.hota002@gmail.com>
2018-04-30 12:10:15 +0530

    All Components are created

--- WebLinks/src/app/app.module.ts
+++ WebLinks/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HomeComponent } from './components/home/home.component';
+import { EventsComponent } from './components/events/events.component';
+import { UsersComponent } from './components/users/users.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    EventsComponent,
+    UsersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
